bd11bcd8d9015a6b58f33e94d6ce39ed19071fb6
T3AD20 <T3AD20@T3AD20C>
2017-01-02 15:42:50 +0900

    1st commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+/**
+ * 
+ * @author T3AD20
+ *
+ */
+public class HelloWorld {
+
+	public static void main2(String[] args) {
+		// Hello World 출력
+		System.out.println( "Hello World" );
+	}
+}
