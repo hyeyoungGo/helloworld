38d204f8694e402f7f3f0973071556c93d319b12
T3AD20 <T3AD20@T3AD20C>
2017-01-02 15:45:41 +0900

    main 함수 오류 수정 변수 s 추가

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -7,8 +7,9 @@ package helloworld;
  */
 public class HelloWorld {
 
-	public static void main2(String[] args) {
+	public static void main(String[] args) {
+		String s = "Hello World";
 		// Hello World 출력
-		System.out.println( "Hello World" );
+		System.out.println( s );
 	}
 }
